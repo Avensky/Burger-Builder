64e561e9202db9e9b0a48254804469a23439f77d
uriel zacarias <urielzacarias@gmail.com>
2020-03-05 20:36:39 -0800

    added order button that is disabled until toppings are selected

--- containers/BurgerBuilder/BurgerBuilder.jsx
+++ containers/BurgerBuilder/BurgerBuilder.jsx
@@ -22,7 +22,20 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 6.0
+        totalPrice: 6.0,
+        purchaseable: false
+    }
+
+    updatePurchaseState (ingredients) {
+//        const ingredients = {//
+//            ...this.state.ingredients
+//        }
+        const sum = Object.keys(ingredients).map( igKey =>{
+                return ingredients[igKey]
+            }).reduce((sum, el) => {
+                return sum + el;
+            },0);
+        this.setState({purchaseable: sum>0});
     }
 
     addIngredientHandler = (type) => {
@@ -36,6 +49,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler =(type) => {
@@ -52,7 +66,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updatedIngredients})
-
+        this.updatePurchaseState(updatedIngredients);
     }
 
     render () {
@@ -69,7 +83,9 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
                     price={this.state.totalPrice}
+
                 />
             </Auxiliary>
         );
