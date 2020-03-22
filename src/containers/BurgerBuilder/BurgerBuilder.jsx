afae58e01e4b3f23dcd0fd5966bf101f6dc1ed06
uriel zacarias <urielzacarias@gmail.com>
2020-03-21 20:03:41 -0700

    enable order button when ingredients get added

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -16,7 +16,6 @@ class BurgerBuilder extends Component {
         this.state={...}
     } */
     state = {
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -45,7 +44,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             },0);
-        this.setState({purchaseable: sum>0});
+        return sum > 0
     }
 
     purchaseHandler = () => {
@@ -90,7 +89,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.price}
                     />
