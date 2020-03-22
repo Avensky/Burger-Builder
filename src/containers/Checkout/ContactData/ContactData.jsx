9e771b8c75c366977bb9cc68a3fcf02d41ca97d7
uriel zacarias <urielzacarias@gmail.com>
2020-03-21 20:29:16 -0700

    connected the checkout componenet and contact data component to the store

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -4,6 +4,7 @@ import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 import Input from '../../../components/UI/Input/Input';
+import {connect} from 'react-redux';
 class ContactData extends Component {
 
     state = {
@@ -15,12 +16,13 @@ class ContactData extends Component {
         },
         loading: false
     }
+
     orderHandler = (event) => {
         event.preventDefault();
         console.log(this.props.ingredients);
         this.setState({loading: true})
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             customer: {
                 name: 'Uriel Zacarias',
@@ -65,4 +67,11 @@ class ContactData extends Component {
 
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
