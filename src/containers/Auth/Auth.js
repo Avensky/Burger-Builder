2c2b16a6fcfbf58fa2509b3096b7ae40b34cde1c
uriel zacarias <urielzacarias@gmail.com>
2020-03-24 02:51:38 -0700

    set up sign up with a server

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -170,7 +170,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-//        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) ),
+        onAuth: ( email, password, isSignup ) => dispatch( actions.auth( email, password, isSignup ) ),
 //        onSetAuthRedirectPath: () => dispatch(actions.setAuthRedirectPath('/'))
     };
 };
