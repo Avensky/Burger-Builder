9ba2c84fc8b3660e08d0bfc26844302bba87914a
uriel zacarias <urielzacarias@gmail.com>
2020-03-24 12:50:06 -0700

    app now updates the navigation bar based on authentication

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -9,7 +9,7 @@ const toolbar = (props) => (
         <DrawerToggle clicked={props.drawerToggleClicked}/>
         <Logo height="95%" />
         <nav className={classes.DesktopOnly}>
-            <NavigationItems />
+            <NavigationItems isAuthenticated={props.isAuth}/>
         </nav>
     </header>
 );
