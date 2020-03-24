55425096f6a608c339fe959f0c55445ea40ce140
uriel zacarias <urielzacarias@gmail.com>
2020-03-24 13:30:45 -0700

    users are fowarded to authentication before placing order

--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -39,9 +39,13 @@ class BurgerBuilder extends Component {
     }
 
     purchaseHandler = () => {
-        this.setState({purchasing: true });
+        if (this.props.isAuthenticated) {
+            this.setState( { purchasing: true } );
+        } else {
+//            this.props.onSetAuthRedirectPath('/checkout');
+            this.props.history.push('/auth');
+        }
     }
-
     purchaseCancelHandler = () => {
         this.setState({purchasing: false });
     }
@@ -71,6 +75,7 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchaseable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
+                        isAuth={this.props.isAuthenticated}
                         price={this.props.price}
                     />
                 </Auxiliary>
@@ -101,7 +106,8 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     }
 }
 
@@ -111,6 +117,7 @@ const mapDispatchToProps = dispatch => {
         onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
         onInitIngredients: () => dispatch(actions.initIngredients()),
         onInitPurchase: () => dispatch(actions.purchaseInit())
+//        onSetAuthRedirectPath: (path) => dispatch(actions.setAuthRedirectPath(path))
     }
 }
 
